f3fed7855033216ada2dbdb2d7e46ed4ebe9d411
Josh <joshua.c.english@gmail.com>
2019-02-13 07:44:01 -0500

    adding files for sync

---
+++ 10 Intro to JavaScript/age.js
@@ -0,0 +1,6 @@
+var age = prompt("How old are you?");
+
+var days = age * 365.25;
+
+alert("You are " + days + " days old!");
+console.log("You are " + days + " days old!");
---
+++ 10 Intro to JavaScript/stScript.js
@@ -0,0 +1,7 @@
+var first = prompt("What is your first name?");
+var last = prompt("What is your last name?");
+var age = prompt("How old are you?");
+alert("Hello, " + first + " " + last);
+console.log("Hello, " + first + " " + last);
+alert("You are " + age + " years old!");
+console.log("You are " + age + " years old!");
