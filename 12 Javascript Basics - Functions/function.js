95e6eaedd1d8b8a5201411902ceda2fd7ccc13c6
Josh <joshua.c.english@gmail.com>
2019-02-16 15:22:35 -0500

    basic function problem set JS

---
+++ 12 Javascript Basics - Functions/function.js
@@ -0,0 +1,42 @@
+console.log("Problem 1:");
+function isEven(num) {
+	if (num % 2 == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+console.log(isEven(4));
+console.log(isEven(21));
+console.log(isEven(68));
+console.log(isEven(333));
+
+
+console.log("Problem 2:");
+function factorial(num) {
+	var fact = 1;
+	if (num == 0 || num == 1) {
+		return 1;
+	}
+	for (var i = num; i > 0; i--) {
+		fact = fact * i;
+	}
+	return fact;
+}
+
+console.log(factorial(5));
+console.log(factorial(2));
+console.log(factorial(10));
+console.log(factorial(0));
+
+console.log("Problem 3:");
+function kebabToSnake(string) {
+	return string.replace(/-/g, "_");
+}
+
+console.log(kebabToSnake("hello-world"));
+console.log(kebabToSnake("dogs-are-awesome"));
+console.log(kebabToSnake("blah"));
+
