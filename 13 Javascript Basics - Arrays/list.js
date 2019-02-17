207a623dc3b0b2bd3e047670c614985759764f05
Josh <joshua.c.english@gmail.com>
2019-02-17 13:30:19 -0500

    refactor todo list for better ui with JS forEach

--- 13 Javascript Basics - Arrays/list.js
+++ 13 Javascript Basics - Arrays/list.js
@@ -1,17 +1,41 @@
 var todos = [];
+
+function listTodos() {
+	console.log("**********");
+	todos.forEach(function(todo, index) {
+		console.log(index + ": " + todo);
+	});
+	console.log("**********");
+}
+
+function newTodo() {
+	var todo = prompt("Enter a new todo:");
+	todos.push(todo);
+	console.log("Added todo!");
+}
+
+function deleteTodo() {
+	var index = prompt("Enter index of todo to delete:");
+	todos.splice(index,1);
+	console.log("Deleted todo!");
+}
+
 window.setTimeout(function() {
 	var quit = false;
 	while (!quit) {
 		var input = prompt("What would you like to do?");
 		if (input == "list") {
-			console.log(todos)
+			listTodos();
 		}
 		else if (input == "new") {
-			var todo = prompt("Enter a new todo:");
-			todos.push(todo);
+			newTodo();
+		}
+		else if (input == "delete") {
+			deleteTodo();
 		}
 		else if (input == "quit") {
 			quit = true;
 		}
 	}
+	console.log("User quit the app.")
 }, 500);
