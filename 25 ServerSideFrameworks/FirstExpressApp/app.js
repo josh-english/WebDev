6126d77ff5c97a880de1ecfb478956fed834868e
Josh English <joshua.c.english@gmail.com>
2019-02-28 14:23:16 +0000

    express route params

--- 25 ServerSideFrameworks/FirstExpressApp/app.js
+++ 25 ServerSideFrameworks/FirstExpressApp/app.js
@@ -5,15 +5,33 @@ var app =  express();
 app.get("/", function(req, res) {
     res.send("Hi there!");
 });
+
 // "/bye" => "Goodbye!"
 app.get("/bye", function(req, res) {
    res.send("Goodbye!"); 
 });
+
 // "/dog" => "Meow!"
 app.get("/dog", function(req, res) {
    console.log("Someone made a request to /dog!");
    res.send("Meow!"); 
 });
+
+// route patterns
+app.get("/r/:subredditName", function(req, res) {
+    var subreddit = req.params.subredditName;
+    res.send("Welcome to the " + subreddit + " subreddit!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res) {
+    res.send("Welcome to the comments page!");
+});
+
+// order matters!
+app.get("*", function(req, res) {
+   res.send("You are a star!"); 
+});
+
 
 // Tell express to listen for requests
 app.listen(process.env.PORT, process.env.IP, function() {
