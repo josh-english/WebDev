51b9881d553810c292ce6c5c506bf5f9c5412fed
Josh English <joshua.c.english@gmail.com>
2019-03-12 13:20:47 +0000

    JSON placeholder api test

--- 27 Working with APIs/APIs/first_request.js
+++ 27 Working with APIs/APIs/first_request.js
@@ -1,13 +1,9 @@
 var request = require('request');
 
-request('http://www.google.com', function(error, response, body){
-    if(error) {
-        console.log("Something went wrong!");
-        console.log(error);
-    } else {
-        if (response.statusCode == 200) {
-            //Things worked
-            console.log(body);
-        }
+request('https://jsonplaceholder.typicode.com/users/1', function(error, response, body){
+    if(!error && response.statusCode == 200){
+        var parsedData = JSON.parse(body);
+        console.log("User's name is:")
+        console.log(parsedData['name']);
     }
 });
