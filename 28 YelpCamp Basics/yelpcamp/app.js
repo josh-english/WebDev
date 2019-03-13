423a936c90aae7be15b2f661cc9572acd56da8f5
Josh English <joshua.c.english@gmail.com>
2019-03-13 12:01:52 +0000

    added functionality to add new campground with REST convention

--- 28 YelpCamp Basics/yelpcamp/app.js
+++ 28 YelpCamp Basics/yelpcamp/app.js
@@ -1,19 +1,37 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+            {name: "Salmon Creek", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104490f2c57da5efb6b8_340.jpg"},
+            {name: "Granite Hill", image: "https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144497f4c57ba5eeb5_340.jpg"},
+            {name: "Mountain Goat's Rest", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"}
+        ];
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-            {name: "Salmon Creek", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104490f2c57da5efb6b8_340.jpg"},
-            {name: "Granite Hill", image: "https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144497f4c57ba5eeb5_340.jpg"},
-            {name: "Mountain Goat's Rest", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"}
-        ];
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    // get data from form
+    var name = req.body.name;
+    var image = req.body.image;
+    // add to campgrounds array
+    var newCampground = {name: name, image:image};
+    campgrounds.push(newCampground);
+    // redirect to campgrounds page
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
