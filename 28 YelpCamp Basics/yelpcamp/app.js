81280127b5bd2140dfbf6505457b09e7018debd1
Josh English <joshua.c.english@gmail.com>
2019-03-13 12:26:26 +0000

    yelpcamp bootstrap styling campgrounds page

--- 28 YelpCamp Basics/yelpcamp/app.js
+++ 28 YelpCamp Basics/yelpcamp/app.js
@@ -8,6 +8,12 @@ app.set("view engine", "ejs");
 var campgrounds = [
             {name: "Salmon Creek", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104490f2c57da5efb6b8_340.jpg"},
             {name: "Granite Hill", image: "https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144497f4c57ba5eeb5_340.jpg"},
+            {name: "Mountain Goat's Rest", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"},
+            {name: "Salmon Creek", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104490f2c57da5efb6b8_340.jpg"},
+            {name: "Granite Hill", image: "https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144497f4c57ba5eeb5_340.jpg"},
+            {name: "Mountain Goat's Rest", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"},
+            {name: "Salmon Creek", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104490f2c57da5efb6b8_340.jpg"},
+            {name: "Granite Hill", image: "https://pixabay.com/get/e136b80728f31c22d2524518b7444795ea76e5d004b0144497f4c57ba5eeb5_340.jpg"},
             {name: "Mountain Goat's Rest", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"}
         ];
 
