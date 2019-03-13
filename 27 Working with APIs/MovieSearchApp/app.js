df803eeb47aa52385b51982ca4cd30cd5db447fe
Josh English <joshua.c.english@gmail.com>
2019-03-13 11:17:19 +0000

    finished movie app, start of yelp camp

--- 27 Working with APIs/MovieSearchApp/app.js
+++ 27 Working with APIs/MovieSearchApp/app.js
@@ -4,8 +4,14 @@ var request = require("request");
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+    res.render("search");
+});
+
 app.get('/results', function(req, res) {
-    request('http://omdbapi.com/?s=california&apikey=thewdb', function(error, response, body){
+    var search = req.query.search
+    var url = 'http://omdbapi.com/?s=' + search + '&apikey=thewdb'
+    request(url, function(error, response, body){
         if(!error && response.statusCode == 200) {
             var data = JSON.parse(body);
             res.render("results", {data: data});
