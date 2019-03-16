ab913c33a02416a4cdde8d6582ade50977ac3a16
Josh English <joshua.c.english@gmail.com>
2019-03-16 21:41:51 +0000

    create rest route

--- 31 RESTful Routing/restfulblogapp/app.js
+++ 31 RESTful Routing/restfulblogapp/app.js
@@ -62,6 +62,18 @@ app.post("/blogs", function(req, res){
     });
 });
 
+// show route
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err) {
+            res.redirect("/blogs");
+        }
+        else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Blog Server Started!");
 });
