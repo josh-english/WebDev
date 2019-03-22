55004bc4d09de2862b4a79799b183779bdecaae6
Josh English <joshua.c.english@gmail.com>
2019-03-22 01:14:11 +0000

    yelpcamp added user association to campgrounds

--- 36 Yelpcamp Cleanup/yelpcamp/routes/campgrounds.js
+++ 36 Yelpcamp Cleanup/yelpcamp/routes/campgrounds.js
@@ -17,13 +17,17 @@ router.get("/", function(req, res){
 });
 
 // create - add new campground to db
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
     // get data from form
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
     // add to campgrounds array
-    var newCampground = {name: name, image:image, description: desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image:image, description: desc, author:author};
     // create a new campground and save to db
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
@@ -39,7 +43,7 @@ router.post("/", function(req, res){
 });
 
 // new - show new form to create new campground
-router.get("/new", function(req, res) {
+router.get("/new", isLoggedIn, function(req, res) {
     res.render("campgrounds/new");
 });
 
@@ -58,5 +62,13 @@ router.get("/:id", function(req, res) {
         }
     });
 });
+
+// middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
