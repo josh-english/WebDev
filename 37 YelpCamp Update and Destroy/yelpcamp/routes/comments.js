6d72bec561e6642917cc5fcfa58d1df62b823ce8
Josh English <joshua.c.english@gmail.com>
2019-03-22 12:34:22 +0000

    yelpcamp comment and campground edit, update, and destroy with user authorization

---
+++ 37 YelpCamp Update and Destroy/yelpcamp/routes/comments.js
@@ -0,0 +1,111 @@
+var express = require("express");
+var router = express.Router({mergeParams: true});
+var Campground = require("../models/campground.js");
+var Comment = require("../models/comment.js");
+
+// comments new
+router.get("/new", isLoggedIn, function(req, res) {
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        }
+        else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+// comments create
+router.post("/", isLoggedIn, function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        }
+        else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                }
+                else {
+                    // add username and id to comment
+                    comment.author.id = req.user.id;
+                    comment.author.username = req.user.username;
+                    // save comment
+                    comment.save();
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + req.params.id);
+                }
+            });
+        }
+    })
+});
+
+// comments edit route
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err){
+            res.redirect("back");
+        }
+        else {
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+        }
+    });
+});
+
+// comments update route
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if(err){
+            res.redirect("back");
+        }
+        else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+// comment delete route
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back");
+        }
+        else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+function checkCommentOwnership(req, res, next){
+    // is user logged in
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back");
+            }
+            else {
+                // does the user own the comment
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                }
+                else {
+                    res.redirect("back");
+                }
+            }
+        });
+    }
+    else {
+        res.redirect("back");
+    }
+}
+
+// middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
---
+++ 37 YelpCamp Update and Destroy/yelpcamp/seeds.js
@@ -0,0 +1,78 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var data = [
+    {
+        name: "Cloud's Rest",
+        image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet tellus malesuada, gravida dui id, tincidunt libero. Nunc faucibus ipsum eget magna vulputate feugiat. Donec ullamcorper justo eu arcu scelerisque iaculis. Nulla tristique elit sem, sed egestas purus vulputate in. Nam egestas mauris at neque faucibus, eget tempor lacus varius. Aenean suscipit vestibulum iaculis. Suspendisse potenti. Praesent sit amet ante nec nisi gravida fringilla a eu turpis.",
+        author: {
+            id: "588c2e092403d111454fff76",
+            username: "Jack"
+        }
+    },
+    {
+        name: "Desert Mesa",
+        image: "https://images.unsplash.com/photo-1484960055659-a39d25adcb3c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+        description: "Vivamus suscipit magna eu tellus molestie, ut feugiat tellus vehicula. Integer quis placerat tellus. Vestibulum semper eu urna vel scelerisque. Aenean porta in sem venenatis finibus. Nunc id eros lacus. Curabitur est ante, finibus non molestie ac, convallis laoreet massa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pretium imperdiet sem sed hendrerit. Fusce diam neque, auctor a nisi at, placerat commodo turpis. Nullam faucibus posuere magna, vel luctus sem imperdiet vitae. Suspendisse potenti. Suspendisse et ligula vitae felis lobortis finibus. Donec velit neque, finibus ac laoreet eu, blandit non sem. Praesent odio libero, blandit in faucibus ac, ultrices quis nunc.",
+        author:{
+            id : "588c2e092403d111454fff71",
+            username: "Jill"
+        }
+    },
+    {
+        name: "Canyon Floor",
+        image: "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1259&q=80",
+        description: "In sit amet leo eleifend, rutrum nisi et, blandit nunc. Donec odio orci, tincidunt et nulla in, elementum sollicitudin ex. Suspendisse ultricies ipsum vel ante porta, et varius velit vulputate. In hac habitasse platea dictumst. Aliquam vitae eleifend enim. Morbi tempus ultricies nibh, congue aliquet ipsum dictum at. Fusce pharetra congue neque, sit amet malesuada ante gravida at. Curabitur elementum tincidunt magna a ultricies. Maecenas erat risus, faucibus eu tincidunt ac, blandit ut neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque quis nisi vel leo luctus venenatis. Pellentesque sit amet erat est. Sed eu arcu posuere, laoreet ante ullamcorper, vestibulum orci.",
+        author:{
+            id : "588c2e092403d111454fff77",
+            username: "Jane"
+        }
+    }
+]
+
+function seedDB(){
+    // remove all campgrounds
+    Campground.deleteMany({}, function(err){
+        if(err) {
+            console.log(err);
+        }
+        console.log("removed campgrounds");
+        Comment.deleteMany({}, function(err){
+            if(err){
+                console.log(err);
+            }
+            console.log("removed comments");
+            // add a few campgrounds
+            data.forEach(function(seed){
+                Campground.create(seed, function(err, campground){
+                    if(err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log("added a campground");
+                        Comment.create({
+                            text: "This place is great, but I wish there was internet",
+                            author:{
+                                id : "588c2e092403d111454fff76",
+                                username: "Jack"
+                            }
+                        }, function(err, comment){
+                            if(err){
+                                console.log(err);
+                            }
+                            else {
+                                campground.comments.push(comment);
+                                campground.save();
+                                console.log("created a comment");
+                            }
+                        });
+                    }
+                });
+            });
+        });
+    });
+}
+
+module.exports = seedDB;
