484be6e045570355f8dd9e3c782bc2376830c60c
Josh English <joshua.c.english@gmail.com>
2019-03-26 14:10:27 +0000

    update dburl

--- 40 Deploying/yelpcamp/app.js
+++ 40 Deploying/yelpcamp/app.js
@@ -18,7 +18,7 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb+srv://josh:14659y3nV@cluster0-oqnle.mongodb.net/yelp_camp?retryWrites=true", {useNewUrlParser: true})
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true})
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
