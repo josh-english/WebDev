ed453c8b5a38c32cd614d197b84567fdb9a688c4
Josh English <joshua.c.english@gmail.com>
2019-03-26 13:55:11 +0000

    add mongodb atlas cluster url for mongoose connection

--- 40 Deploying/yelpcamp/app.js
+++ 40 Deploying/yelpcamp/app.js
@@ -18,7 +18,7 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true})
+mongoose.connect("mongodb+srv://josh:14659y3nV@cluster0-oqnle.mongodb.net/yelp_camp?retryWrites=true", {useNewUrlParser: true})
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
